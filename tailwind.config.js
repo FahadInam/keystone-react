b941c837372cf5bfa007b4b7c83e363b464260cc
FahadInam <fahadinam4@gmail.com>
2023-03-25 02:39:54 +0500

    signin page updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        onboardingShadow: '0px 3px 11px rgba(0, 0, 0, 0.06), 0px 10px 15px rgba(0, 0, 0, 0.03)',
+      },
+      colors: {
+        primarybtn: '#60A498',
+        primarytext: '#60A498',
+      },
+    },
   },
   plugins: [],
 }
